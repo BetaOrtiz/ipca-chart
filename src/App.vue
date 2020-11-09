f71de8697e5b7bee761369b4b0df27ee622ea8e3
BetaOrtiz <emaildabetaortiz@gmail.com>
2020-11-09 18:35:28 -0300

    feat: cards index added

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,36 @@
     <div id="header">
       <div class="header-content">
         <div class="logo">
-          <img src="./assets/cef.svg" alt="Caixa Econômica" />
+          <img src="./assets/cef_neg.png" alt="Caixa Econômica" />
         </div>
         <div class="header-elements">
-          <div id="header-title">
-            <h1>IPCA</h1>
-            <h2>Consulta Histórico</h2>
-          </div>
-          <div class="nav">
-            <router-link to="/">
-              <button class="main-buttons" @click="page = 'table'">
-                TABELA HISTÓRICA
-              </button></router-link
-            >
+          <div class="header-title">
+            <div class="title">
+              <h1>IPCA</h1>
+              <h2>Consulta Histórico</h2>
+              <router-link to="/">
+                <button class="main-buttons" @click="page = 'table'">
+                  <span> TABELA HISTÓRICA </span>
+                </button></router-link
+              >
 
-            <router-link to="/chart">
-              <button class="main-buttons" @click="page = 'chart'">
-                ANÁLISE POR GRUPOS
-              </button>
-            </router-link>
+              <router-link to="/chart">
+                <button class="main-buttons" @click="page = 'chart'">
+                  <span> ANÁLISE POR GRUPOS </span>
+                </button>
+              </router-link>
+            </div>
+            <div class="indexes">
+              <v-card
+                v-for="(item, key) in indexes"
+                :key="key"
+                class="card"
+                elevation="2"
+              >
+                <span>{{ item.variavel }}</span>
+                <p>{{ item.serie }}</p>
+              </v-card>
+            </div>
           </div>
         </div>
       </div>
@@ -46,6 +57,17 @@ import '../src/styles/global.css';
 export default {
   methods: {
     getDate() {
+      const date = new Date();
+      let month = date.getMonth().toString();
+
+      const serializedMonth =
+        month.length === 2 ? month : `0${month}`;
+
+      const year = date.getFullYear();
+
+      return `${year}${serializedMonth}`;
+    },
+    getMonths() {
       const meses = [...new Set(this.ipcaData.map(item => item.D3N))];
 
       const mesesUpper = meses.map(mes => mes.toUpperCase());
@@ -73,6 +95,7 @@ export default {
       mesesDisponiveis: [],
       groups: [],
       ipcaData: [],
+      indexes: [],
     };
   },
 
@@ -80,7 +103,7 @@ export default {
     this.$http.get('/').then(res => {
       this.ipcaData = res.data;
 
-      let x = this.getDate();
+      let x = this.getMonths();
       x.pop();
       this.mesesDisponiveis = x;
       this.selectedDate = this.mesesDisponiveis[0];
@@ -93,6 +116,22 @@ export default {
       x.shift();
       this.groups = x;
     });
+
+    const date = this.getDate();
+
+    this.$http
+      .get(
+        `https://servicodados.ibge.gov.br/api/v3/agregados/7060/periodos/${date}/variaveis/63|69?localidades=N1[all]&classificacao=315[7169]`,
+      )
+      .then(res => {
+        res.data.map(item => {
+          const dataIndice = {
+            variavel: item.variavel,
+            serie: item.resultados[0].series[0].serie[date],
+          };
+          this.indexes = [...this.indexes, dataIndice];
+        });
+      });
   },
 };
 </script>
